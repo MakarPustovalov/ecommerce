6c0a1ed08e637ed5cbd74b36d6ba4aa5c1c27727
makarpustovalov <makar.pht@gmail.com>
2021-09-21 12:43:35 +0300

    crud for brand

--- server/controllers/BrandController.js
+++ server/controllers/BrandController.js
@@ -5,36 +5,70 @@ class BrandController {
 
   async create(req, res, next) {
     try {
+
       const {name} = req.body
       if (!name) return next(ApiError.badRequest('Name is not provided'))
+
       let duplicate = await BrandModel.findOne({name})
       if (duplicate) return next(ApiError.badRequest('Brand with this name already excists'))
+
       let brand = await new BrandModel({name})
       brand = await brand.save()
       if (!brand) return next(ApiError.internalError('Something went wrong'))
-      return res.json({result: brand})
+      return res.json({success: true, result: brand})
+
     } catch (error) {
       console.log(error)
       return next(ApiError.internalError('Something went wrong'))
     }
   }
 
-  async getAll (req, res) {
+  async getAll (req, res, next) {
     try {
+
       let result = await BrandModel.find({})
-      return res.json({result: result})
+      return res.json({success: true, result: result})
+
     } catch (error) {
       console.log(error)
       return next(ApiError.internalError('Something went wrong'))
     }
   }
   
-  async update (req, res) {
+  async update (req, res, next) {
+    try {
 
+      const {name, newName} = req.body
+      if (!name) return next(ApiError.badRequest('Name is not provided'))
+      if (!newName) return next(ApiError.badRequest('New name is not provided'))
+
+      let duplicate = await BrandModel.findOne({name: newName})
+      if (duplicate) return next(ApiError.badRequest('Brand with this name already excists'))
+
+      let brand = await BrandModel.findOneAndUpdate({name}, {name: newName}, {new: true})
+      if (!brand) return next(ApiError.internalError('Bad request'))
+      return res.json({success: true, result: brand})
+
+    } catch (error) {
+      console.log(error)
+      return next(ApiError.internalError('Something went wrong'))
+    }
   }
   
-  async delete (req, res) {
-    
+  async delete (req, res, next) {
+    try {
+
+      const {name} = req.body
+      if (!name) return next(ApiError.badRequest('Name is not provided'))
+
+      let brand = await BrandModel.findOneAndDelete({name})
+      if (!brand) return next(ApiError.internalError('Brand with this name does not excist'))
+      return res.json({success: true, result: brand})
+
+    } catch (error) {
+      console.log(error)
+      return next(ApiError.internalError('Something went wrong'))
+    }
   }
   
 }
